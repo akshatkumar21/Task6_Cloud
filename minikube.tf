b7a40b5d5bab62db51e1e9c31c17dcc61ffda2b1
Akshat Kumar <akshatkumar628@gmail.com>
2020-09-17 20:15:24 +0530

    first

---
+++ minikube.tf
@@ -0,0 +1,70 @@
+provider "kubernetes" {
+	config_context_cluster = "minikube"
+}
+
+resource "null_resource" "minikube" {
+
+	provisioner "local-exec" {
+        command = "minikube start"
+	}
+}
+
+resource "kubernetes_deployment" "wordpress" {
+	metadata {
+    	name = "wp"
+    }
+ 
+	spec {
+	  replicas = 1
+	  selector {
+	  	match_labels = {
+	    	env = "production"
+		    region = "IN"
+		    App = "wordpress"
+	    }
+
+		match_expressions {
+	        key = "env"
+		    operator = "In"
+		    values = ["production" , "webserver"]
+		}
+      }
+	template {
+    	metadata {
+        	labels = {
+    			env = "production"
+    			region = "IN"
+    			App = "wordpress"
+   			}
+  		}
+
+        spec {
+        	container {
+        		image = "wordpress:4.8-apache"
+    			name = "wp" 
+    		}
+   		}
+  	}
+  }
+}
+
+resource "kubernetes_service" "mywordpress" {
+	metadata {
+  		name = "wordpress"
+    }
+ 
+    spec {
+  		selector = {
+   			App = "wordpress"
+  	    }
+
+	  	port {
+		   	node_port   = 31010
+		   	port = 80
+		   	target_port = 80
+	    }
+  
+  		type = "NodePort"
+    }
+}
+
